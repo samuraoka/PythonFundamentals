6e807b794d9a89e6ac653e3d9aa72b4aa0b2d557
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-11-28 04:05:11 +0900

    Scalar Types: int, float, None and bool

--- PythonFundamentals/PythonFundamentals.py
+++ PythonFundamentals/PythonFundamentals.py
@@ -32,3 +32,38 @@ k = 2
 fac(n) // (fac(k) * fac(n - k))
 fac(n)
 len(str(fac(n)))
+
+10
+0b10
+0o10
+0x10
+int(3.5)
+int(-3.5)
+int("496")
+int("10000", 3)
+3.125
+3e8
+1.616e-35
+float(7)
+float("1.618")
+float("nan")
+float("inf")
+float("-inf")
+3.0 + 1
+None
+a = None
+a is None
+True
+False
+bool(0)
+bool(42)
+bool(-1)
+bool(0.0)
+bool(0.207)
+bool(-1.117)
+bool([])
+bool([1, 5, 9])
+bool("")
+bool("Spam")
+bool("True")
+bool("False")
