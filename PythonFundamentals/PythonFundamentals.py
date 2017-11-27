8091b0a6be8683ce8e78cb1428e46fbca188fa3f
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-11-27 17:49:32 +0900

    Importing From the Python Standard Library

--- PythonFundamentals/PythonFundamentals.py
+++ PythonFundamentals/PythonFundamentals.py
@@ -6,3 +6,29 @@ x
 _
 _ * 2
 print('Hello, Python')
+
+for i in range(5):
+    x = i * 10
+    print(x)
+
+import math
+math.sqrt(81)
+help(math.sqrt)
+help(math.factorial)
+math.factorial(5)
+math.factorial(6)
+n = 5
+k = 3
+math.factorial(n) / (math.factorial(k) * math.factorial(n-k))
+from math import factorial
+factorial(n) / (factorial(k) * factorial(n - k))
+from math import factorial as fac
+fac(n) / (fac(k) * fac(n - k))
+2**31 - 1 
+fac(12)
+fac(13)
+n = 100
+k = 2
+fac(n) // (fac(k) * fac(n - k))
+fac(n)
+len(str(fac(n)))
