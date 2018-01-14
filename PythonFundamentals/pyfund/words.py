381adaf5782a978fe7d4a1d737d312372e96b6dd
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-01-15 00:03:07 +0900

    Implemented some code: Main Functions and Command Line Arguments
    Python Fundamentals module 4 - Modularity

--- PythonFundamentals/pyfund/words.py
+++ PythonFundamentals/pyfund/words.py
@@ -1,14 +1,35 @@
+import sys
 from pip._vendor.requests.packages import urllib3
 
-# urllib3 User Guide
-# https://urllib3.readthedocs.io/en/latest/user-guide.html
-http = urllib3.PoolManager()
+def fetch_words(url):
+    # Working with Python in Visual Studio (Windows)
+    # https://docs.microsoft.com/en-us/visualstudio/python/python-in-visual-studio
+    #
+    # urllib3 User Guide
+    # https://urllib3.readthedocs.io/en/latest/user-guide.html
+    #
+    # How to know/change current directory in Python shell?
+    # https://stackoverflow.com/questions/8248397/how-to-know-change-current-directory-in-python-shell
+    http = urllib3.PoolManager()
 
-with http.request('GET', 'http://sixty-north.com/c/t.txt') as story:
-    print(story.status)
-    story_words = []
-    for word in story.data.decode('utf-8').split():
-        story_words.append(word)
+    with http.request('GET', url) as story:
+        print(story.status)
+        story_words = []
+        for word in story.data.decode('utf-8').split():
+            story_words.append(word)
+    return story_words
 
-for word in story_words:
-    print(word)
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__ == '__main__':
+    # 'http://sixty-north.com/c/t.txt'
+    main(sys.argv[1])
