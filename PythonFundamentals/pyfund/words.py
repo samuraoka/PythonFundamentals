3936b13c646248e5dfd1b7eedbc35fb33d52e357
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-01-15 01:48:55 +0900

    Implemented some code: Documenting Your Code With Comments
    Python Fundamentals
    Module 4 - Modularity

--- PythonFundamentals/pyfund/words.py
+++ PythonFundamentals/pyfund/words.py
@@ -1,7 +1,22 @@
+"""Retrieve and print words from a URL.
+
+Usage:
+
+    python words.py <URL>
+"""
 import sys
 from pip._vendor.requests.packages import urllib3
 
 def fetch_words(url):
+    """Fetch a list of words from a URL
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing words from
+        the document.
+    """
     # Working with Python in Visual Studio (Windows)
     # https://docs.microsoft.com/en-us/visualstudio/python/python-in-visual-studio
     #
@@ -10,6 +25,10 @@ def fetch_words(url):
     #
     # How to know/change current directory in Python shell?
     # https://stackoverflow.com/questions/8248397/how-to-know-change-current-directory-in-python-shell
+    #
+    # Google Python Style Guide - Comments
+    # https://google.github.io/styleguide/pyguide.html?showone=Comments#Comments
+    #
     http = urllib3.PoolManager()
 
     with http.request('GET', url) as story:
@@ -21,15 +40,25 @@ def fetch_words(url):
 
 
 def print_items(items):
+    """Print items one per line.
+
+    Args:
+        items: An iterable series of printable items
+    """
     for item in items:
         print(item)
 
 
 def main(url):
+    """Print each word from a text document from a URL
+
+    Args:
+        url: The URL of the UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
 
 
 if __name__ == '__main__':
     # 'http://sixty-north.com/c/t.txt'
-    main(sys.argv[1])
+    main(sys.argv[1]) # The 0th arg is the module filename
